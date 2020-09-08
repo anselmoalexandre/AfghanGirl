a7de32b7f561b99c4775fc4beb83f4cefebb484e
Anselmo Alexandre <anselmoalexandre@bilheteira.co.mz>
2020-09-08 10:50:34 +0200

    Afghan Girl - Added the start destination all in the same Navigation Graph just because it was not working on separeted nav graphs files

--- app/src/main/java/mz/co/bilheteira/afghangirl/Main.kt
+++ app/src/main/java/mz/co/bilheteira/afghangirl/Main.kt
@@ -1,23 +1,34 @@
 package mz.co.bilheteira.afghangirl
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.content_main.*
 
 class Main : AppCompatActivity() {
+
+    // NavHost
+    private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.nav_host_fragment) }
+
+    // NavController
+    private val navController by lazy { (navHostFragment as NavHostFragment).navController }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        // Setting up the Bottom Navigation using Navigation Component
+        //val bottomNav = findViewById<BottomNavigationView>(R.id.main_nav)
+        //setupBottomNav(navController = navController, bottomNav = bottomNav)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -30,9 +41,32 @@ class Main : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        return item.onNavDestinationSelected(navController)
     }
+
+    /**
+     * Setup the Bottom Navigation View
+     * [navController] Navigation Controller
+     * [bottomNav] Bottom Navigation View
+     */
+    //private fun setupBottomNav(navController: NavController, bottomNav: BottomNavigationView) {
+      //  bottomNav.setupWithNavController(navController)
+    //}
+
+    /**
+     * Setup the Navigation View
+     * [navController] Navigation Controller
+     * [navView] Navigation View
+     */
+    //private fun setupDrawerLayout(navController: NavController, navView: NavigationView) {
+      //  navView.setupWithNavController(navController)
+    //}
+
+    /**
+     * Setup Action bar with Navigation Controller
+     * [navController] Navigation Controller
+     */
+    //private fun setupAppBarLayout(navController: NavController) {
+      //  setupActionBarWithNavController(navController)
+    //}
 }
