02ca8216d1cbc6ccb3689585f0b992e7530d22de
Anselmo Alexandre <anselmoalexandre@bilheteira.co.mz>
2020-10-19 12:33:03 +0200

    Designing Explore tab - 03

--- app/src/main/java/mz/co/bilheteira/afghangirl/ui/fragments/Explore.kt
+++ app/src/main/java/mz/co/bilheteira/afghangirl/ui/fragments/Explore.kt
@@ -1,5 +1,9 @@
 package mz.co.bilheteira.afghangirl.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import mz.co.bilheteira.afghangirl.R
 
@@ -8,4 +12,17 @@ import mz.co.bilheteira.afghangirl.R
  * Use the [Explore.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Explore : Fragment(R.layout.fragment_explore)
+class Explore : Fragment(R.layout.fragment_explore) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_explore, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Here goes adapter and other staff
+    }
+}
